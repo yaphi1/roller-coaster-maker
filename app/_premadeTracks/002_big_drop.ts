a56e1eb90a39d027790e761a7e3a732abd7d0b2f
Yaphi Berhanu <yaphi1@gmail.com>
2023-10-16 00:59:20 -0400

    I liked a track I made in the editor so I saved it as a preset

---
+++ app/_premadeTracks/002_big_drop.ts
@@ -0,0 +1,38 @@
+import { PieceType } from "../_utils/types";
+
+const track: PieceType[] = [
+  "straight",
+  "straight",
+  "straight",
+  "straight",
+  "right",
+  "right",
+  "up",
+  "straight",
+  "straight",
+  "straight",
+  "down",
+  "down",
+  "straight",
+  "straight",
+  "straight",
+  "up",
+  "up",
+  "straight",
+  "down",
+  "left",
+  "left",
+  "down",
+  "straight",
+  "up",
+  "up",
+  "down",
+  "down",
+  "up",
+  "left",
+  "straight",
+  "straight",
+  "right"
+];
+
+export default track;
