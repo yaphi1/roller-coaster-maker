9b1d1938375bdc5060183497deb3c9cc413d3272
Yaphi Berhanu <yaphi1@gmail.com>
2023-10-24 02:29:36 -0400

    Improve pole placement and make connection points look welded

--- app/_scripts/trackBuilder/trackPieceBuilders/buildTrackSupports.tsx
+++ app/_scripts/trackBuilder/trackPieceBuilders/buildTrackSupports.tsx
@@ -3,7 +3,7 @@ import { ColorContext } from "@/app/_components/App";
 import { useContext } from "react";
 import { Path, PathVisual } from '../../types';
 import { getPointsOffsetFromPath } from './getPointsOffsetFromPath';
-import { desiredLengthBetweenPathPoints, trackPieceDepth } from '../trackConstants';
+import { trackPieceDepth } from '../trackConstants';
 import { globalSettings } from '../../globalSettings';
 import { getPathPoints } from './getPathPoints';
 
@@ -13,8 +13,8 @@ function getSupportPolePaths(
   { path, depthOfTopPoint } :
   { path: Path, depthOfTopPoint: number }
 ) {
-  const centerRailPoints = getPointsOffsetFromPath(path, 0, -depthOfTopPoint);
-  const supportPolePoints = centerRailPoints.filter(isIndexSpacedOutEnough);
+  const pathPoints = getPointsOffsetFromPath(path, 0, 0);
+  const supportPolePoints = pathPoints.filter(isIndexSpacedOutEnough);
 
   const supportPolePaths = supportPolePoints.map((supportPolePoint, i) => {
     const groundPoint = new THREE.Vector3(...[
@@ -22,7 +22,12 @@ function getSupportPolePaths(
       0 - depthOfTopPoint,
       supportPolePoint.z,
     ]);
-    const polePath = new THREE.LineCurve3(supportPolePoint, groundPoint);
+    const supportPoleTop = new THREE.Vector3(
+      supportPolePoint.x,
+      supportPolePoint.y - depthOfTopPoint,
+      supportPolePoint.z,
+    );
+    const polePath = new THREE.LineCurve3(supportPoleTop, groundPoint);
 
     return polePath;
   });
@@ -60,19 +65,24 @@ export function buildTrackSupports(trackPath: Path): PathVisual[] {
   const radialSegments = 8;
 
   const trackPathPoints = getPathPoints(trackPath);
-  const supportPaths = getSupportPolePaths({ path: trackPath, depthOfTopPoint: trackPieceDepth + 0.1 });
+  const supportPaths = getSupportPolePaths({ path: trackPath, depthOfTopPoint: trackPieceDepth + 0.2 });
 
   const supports = supportPaths.map((supportPath, i) => {
     const isValid = isValidSupport(supportPath, trackPathPoints);
-    const { x, z } = supportPath.v1;
+    const { x, y, z } = supportPath.v1;
 
     const TrackSupport = () => {
       const scaffoldingColor = useContext(ColorContext)?.coasterColors[0]?.scaffolding;
       const supportMaterial = <meshStandardMaterial color={scaffoldingColor} side={THREE.DoubleSide} wireframe={globalSettings.isDebugMode} />;
       const supportBaseMaterial = <meshStandardMaterial color={scaffoldingColor} wireframe={globalSettings.isDebugMode} />;
+      const weldRadius = 0.16;
 
       return isValid ? (
         <group key={i}>
+          <mesh position={[x, y, z]}>
+            <sphereGeometry args={[weldRadius]} />
+            {supportMaterial}
+          </mesh>
           <mesh>
             <tubeGeometry args={[supportPath, tubularSegments, radius, radialSegments]} />
             {supportMaterial}
